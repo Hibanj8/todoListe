b3710103328b7d6a3ded2b260902c97cce3ad031
SALMA EL HANI <salmaelhani7@gmail.com>
2024-02-01 14:55:27 +0100

    Test

--- src/Components/AddNewTodoForm.jsx
+++ src/Components/AddNewTodoForm.jsx
@@ -117,7 +117,7 @@ return(
                         <label className="block text-sm font-medium text-gray-700" >
                          description <span className="text-red-500">*</span>
                         </label>
-                        <div className="mt-1">
+                        <div      className="mt-1">
                             <input
                                 className="appearance-none block w-full px-3 py-2 border border-bg-teal-700 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-bg-teal-300 sm:text-sm"
                                 required=""
